c38a91aac6701828c0847fd012d2e9c523504bf5
huguesva <43318922+huguesva@users.noreply.github.com>
2024-04-10 17:59:48 +0200

    correct init utils

--- torchdr/utils/__init__.py
+++ torchdr/utils/__init__.py
@@ -1,9 +1,9 @@
-
 # Author: Rémi Flamary <remi.flamary@polytechnique.edu>
+#         Hugues Van Assel <vanasselhugues@gmail.com>
 #
 # License: BSD 3-Clause License
 
 
-from .root_finding import binary_search, false_position, init_bounds
+from .optim import binary_search, false_position, init_bounds
 
-__all__ = ['binary_search', 'false_position', 'init_bounds']
+__all__ = ["binary_search", "false_position", "init_bounds"]
