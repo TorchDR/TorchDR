d5d67aefc5871c9a21f305e7a4c17377737ccf0b
huguesva <43318922+huguesva@users.noreply.github.com>
2024-07-01 09:17:23 +0200

    stable

---
+++ torchdr/neighbor_embedding/largevis.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+LargeVis algorithm
+"""
+
+# Author: Hugues Van Assel <vanasselhugues@gmail.com>
+#
+# License: BSD 3-Clause License
+
+from torchdr.neighbor_embedding.base import NeighborEmbedding
+from torchdr.affinity import (
+    L2SymmetricEntropicAffinity,
+    StudentAffinity,
+)
+from torchdr.utils import sum_red
+
+
+class LargeVis(NeighborEmbedding):
+    """
+    Implementation of the LargeVis algorithm introduced in [13]_.
+
+    Parameters
+    ----------
+    perplexity : float
+        Number of 'effective' nearest neighbors.
+        Consider selecting a value between 2 and the number of samples.
+        Different values can result in significantly different results.
+    n_components : int, optional
+        Dimension of the embedding space.
+    lr : float, optional
+        Learning rate for the algorithm, by default 1.0.
+    optimizer : {'SGD', 'Adam', 'NAdam'}, optional
+        Which pytorch optimizer to use, by default 'Adam'.
+    optimizer_kwargs : dict, optional
+        Arguments for the optimizer, by default None.
+    scheduler : {'constant', 'linear'}, optional
+        Learning rate scheduler.
+    scheduler_kwargs : dict, optional
+        Arguments for the scheduler, by default None.
+    init : {'random', 'pca'} or torch.Tensor of shape (n_samples, output_dim), optional
+        Initialization for the embedding Z, default 'pca'.
+    init_scaling : float, optional
+        Scaling factor for the initialization, by default 1e-4.
+    tol : float, optional
+        Precision threshold at which the algorithm stops, by default 1e-4.
+    max_iter : int, optional
+        Number of maximum iterations for the descent algorithm, by default 100.
+    tolog : bool, optional
+        Whether to store intermediate results in a dictionary, by default False.
+    device : str, optional
+        Device to use, by default "auto".
+    keops : bool, optional
+        Whether to use KeOps, by default False.
+    verbose : bool, optional
+        Verbosity, by default True.
+    random_state : float, optional
+        Random seed for reproducibility, by default 0.
+    coeff_attraction : float, optional
+        Coefficient for the attraction term, by default 10.0 for early exaggeration.
+    coeff_repulsion : float, optional
+        Coefficient for the repulsion term, by default 1.0.
+    early_exaggeration_iter : int, optional
+        Number of iterations for early exaggeration, by default 250.
+    tol_affinity : _type_, optional
+        Precision threshold for the entropic affinity root search.
+    max_iter_affinity : int, optional
+        Number of maximum iterations for the entropic affinity root search.
+    metric_in : {'euclidean', 'manhattan'}, optional
+        Metric to use for the input affinity, by default 'euclidean'.
+    metric_out : {'euclidean', 'manhattan'}, optional
+        Metric to use for the output affinity, by default 'euclidean'.
+    batch_size : int or str, optional
+        Batch size for the optimization, by default None.
+
+    References
+    ----------
+
+    .. [13] Tang, J., Liu, J., Zhang, M., & Mei, Q. (2016).
+            Visualizing Large-Scale and High-Dimensional Data.
+            In Proceedings of the 25th international conference on world wide web.
+
+    """  # noqa: E501
+
+    def __init__(
+        self,
+        perplexity: float = 30,
+        n_components: int = 2,
+        lr: float = 1.0,
+        optimizer: str = "Adam",
+        optimizer_kwargs: dict = None,
+        scheduler: str = "constant",
+        scheduler_kwargs: dict = None,
+        init: str = "pca",
+        init_scaling: float = 1e-4,
+        tol: float = 1e-4,
+        max_iter: int = 1000,
+        tolog: bool = False,
+        device: str = None,
+        keops: bool = False,
+        verbose: bool = True,
+        random_state: float = 0,
+        coeff_attraction: float = 10.0,
+        coeff_repulsion: float = 7.0,
+        early_exaggeration_iter: int = 250,
+        tol_affinity: float = 1e-3,
+        max_iter_affinity: int = 100,
+        metric_in: str = "euclidean",
+        metric_out: str = "euclidean",
+        batch_size: int | str = "auto",
+    ):
+
+        self.metric_in = metric_in
+        self.metric_out = metric_out
+        self.perplexity = perplexity
+        self.max_iter_affinity = max_iter_affinity
+        self.tol_affinity = tol_affinity
+
+        affinity_in = L2SymmetricEntropicAffinity(
+            perplexity=perplexity,
+            metric=metric_in,
+            tol=tol_affinity,
+            max_iter=max_iter_affinity,
+            device=device,
+            keops=keops,
+            verbose=verbose,
+        )
+        affinity_out = StudentAffinity(
+            metric=metric_out,
+            normalization_dim=None,  # normalization is the repulsive loss
+            device=device,
+            keops=keops,
+            verbose=False,
+        )
+
+        super().__init__(
+            affinity_in=affinity_in,
+            affinity_out=affinity_out,
+            n_components=n_components,
+            optimizer=optimizer,
+            optimizer_kwargs=optimizer_kwargs,
+            tol=tol,
+            max_iter=max_iter,
+            lr=lr,
+            scheduler=scheduler,
+            scheduler_kwargs=scheduler_kwargs,
+            init=init,
+            init_scaling=init_scaling,
+            tolog=tolog,
+            device=device,
+            keops=keops,
+            verbose=verbose,
+            random_state=random_state,
+            coeff_attraction=coeff_attraction,
+            coeff_repulsion=coeff_repulsion,
+            early_exaggeration_iter=early_exaggeration_iter,
+        )
+
+    def _repulsive_loss(self, log_Q):
+        Q = log_Q.exp()
+        Q = Q / (Q + 1)  # stabilization trick inspired from UMAP
+        return sum_red(stable.log(), dim=(0, 1))
