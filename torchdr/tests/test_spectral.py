4717120121dde62a539758c5b0c03330a939d819
mathurinm <mathurin.massias@gmail.com>
2024-07-03 08:47:14 +0200

    fix take n_components most important vectors, not least

--- torchdr/tests/test_spectral.py
+++ torchdr/tests/test_spectral.py
@@ -5,20 +5,26 @@ from sklearn.metrics import pairwise_distances
 
 from torchdr.spectral import KernelPCA
 from torchdr.affinity import GibbsAffinity
+from torchdr.utils import center_kernel
 
 
 # def test_fit_transform():
 if __name__ == "__main__":
-    X = torch.randn(20, 30)
+    torch.manual_seed(0)
+    X = torch.randn(4, 5)
+    X /= torch.linalg.norm(X, axis=0)  # otherwise all points at distance 1
     aff = GibbsAffinity(normalization_dim=None, nodiag=False, sigma=1)
-    model = KernelPCA(affinity=aff)
+    model = KernelPCA(affinity=aff, n_components=3)
 
     res_1 = model.fit_transform(X)
     model.fit(X)
     res_2 = model.transform(X)
 
-    np.testing.assert_allclose(res_1, res_2)
-
+    # np.testing.assert_allclose(res_1, res_2)
+    K = center_kernel(aff.fit_transform(X))
+    print(torch.linalg.eigh(K)[0])
+    print(torch.linalg.eigh(K)[1])
+    print(model.eigenvectors_)
     # test = aff.fit_transform(X)
 
     # dist = pairwise_distances(X)
