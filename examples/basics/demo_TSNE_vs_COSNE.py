ede9df72ac805b1c204ac581943a0dd78ac0f13b
Nicolas Courty <ncourty@irisa.fr>
2024-07-12 09:52:31 +0200

    autopep8 example

--- examples/basics/demo_TSNE_vs_COSNE.py
+++ examples/basics/demo_TSNE_vs_COSNE.py
@@ -16,6 +16,9 @@ second one operates in the Hyperbolic Poincaré Ball model.
 #
 # We first construct a synthetic hierarchical dataset with the following class
 
+from torchdr.utils.visu import plotGrid
+from torchdr import TSNE, COSNE
+from torchdr import pairwise_distances
 import torch
 import itertools
 import matplotlib.pylab as plt
@@ -25,7 +28,7 @@ import geoopt
 class SyntheticDataset(torch.utils.data.Dataset):
     '''
     Adopted from https://github.com/emilemathieu/pvae/blob/ca5c4997a90839fc8960ec812df4cbf83da55781/pvae/datasets/datasets.py
-    
+
     Implementation of a synthetic dataset by hierarchical diffusion. 
     Args:
     :param int dim: dimension of the input sample
@@ -35,6 +38,7 @@ class SyntheticDataset(torch.utils.data.Dataset):
     :param float sigma_children: noise
     :param int param: integer by which :math:`\\sigma_children` is divided at each deeper level of the tree
     '''
+
     def __init__(self, ball, dim, depth, numberOfChildren=2, dist_children=1, sigma_sibling=2, param=1, numberOfsiblings=1):
         assert numberOfChildren == 2
         self.dim = int(dim)
@@ -44,11 +48,12 @@ class SyntheticDataset(torch.utils.data.Dataset):
         self.depth = int(depth)
         self.dist_children = dist_children
         self.numberOfChildren = int(numberOfChildren)
-        self.numberOfsiblings = int(numberOfsiblings)  
+        self.numberOfsiblings = int(numberOfsiblings)
         self.__class_counter = itertools.count()
-        self.origin_data, self.origin_labels, self.data, self.labels = map(torch.detach, self.bst())
+        self.origin_data, self.origin_labels, self.data, self.labels = map(
+            torch.detach, self.bst())
         self.num_classes = self.origin_labels.max().item()+1
-        #self.data = ball.mobius_add(self.data, -ball.weighted_midpoint(self.data)).detach()
+        # self.data = ball.mobius_add(self.data, -ball.weighted_midpoint(self.data)).detach()
 
     def __len__(self):
         '''
@@ -83,8 +88,10 @@ class SyntheticDataset(torch.utils.data.Dataset):
             direction = torch.randn_like(parent_value)
             parent_value_n = parent_value / parent_value.norm().clamp_min(1e-15)
             direction -= parent_value_n @ direction * parent_value_n
-            child_value_1 = self.ball.geodesic_unit(torch.tensor(sigma), parent_value, direction)
-            child_value_2 = self.ball.geodesic_unit(torch.tensor(sigma), parent_value, -direction)
+            child_value_1 = self.ball.geodesic_unit(
+                torch.tensor(sigma), parent_value, direction)
+            child_value_2 = self.ball.geodesic_unit(
+                torch.tensor(sigma), parent_value, -direction)
             child_label_1 = parent_label.clone()
             child_label_1[current_depth] = next(self.__class_counter)
             child_label_2 = parent_label.clone()
@@ -95,8 +102,9 @@ class SyntheticDataset(torch.utils.data.Dataset):
             ]
         else:
             children = []
-            for i in range (numberOfChildren):
-                child_value = self.ball.random(self.dim, mean=parent_value, std=sigma ** .5)
+            for i in range(numberOfChildren):
+                child_value = self.ball.random(
+                    self.dim, mean=parent_value, std=sigma ** .5)
                 child_label = parent_label.clone()
                 children.append((child_value, child_label))
         return children
@@ -119,45 +127,51 @@ class SyntheticDataset(torch.utils.data.Dataset):
             if current_depth < self.depth:
                 children = self.get_children(current_node, current_label, current_depth)
                 for child in children:
-                    queue.append((child[0], child[1], current_depth + 1)) 
+                    queue.append((child[0], child[1], current_depth + 1))
             if current_depth <= self.depth:
-                clones = self.get_children(current_node, current_label, current_depth, False)
+                clones = self.get_children(
+                    current_node, current_label, current_depth, False)
                 for clone in clones:
                     values_clones.append(clone[0])
                     labels_clones.append(clone[1])
-        length = int(((self.numberOfChildren) ** (self.depth + 1) - 1) / (self.numberOfChildren - 1))
+        length = int(((self.numberOfChildren) ** (self.depth + 1) - 1) /
+                     (self.numberOfChildren - 1))
         length_leaves = int(self.numberOfChildren**self.depth)
         images = torch.cat([i for i in visited]).reshape(length, self.dim)
-        labels_visited = torch.cat([i for i in labels_visited]).reshape(length, self.depth+1)[:,:self.depth]
-        values_clones = torch.cat([i for i in values_clones]).reshape(self.numberOfsiblings*length, self.dim)
-        labels_clones = torch.cat([i for i in labels_clones]).reshape(self.numberOfsiblings*length, self.depth+1)
+        labels_visited = torch.cat([i for i in labels_visited]).reshape(
+            length, self.depth+1)[:, :self.depth]
+        values_clones = torch.cat([i for i in values_clones]).reshape(
+            self.numberOfsiblings*length, self.dim)
+        labels_clones = torch.cat([i for i in labels_clones]).reshape(
+            self.numberOfsiblings*length, self.depth+1)
         return images, labels_visited, values_clones, labels_clones
 
 # %%
 # Generating the data
 # -----------
 #
-# Let us now generate some data of interest. The dimension of the input space is 
+# Let us now generate some data of interest. The dimension of the input space is
 # set to 50
+
 
 ball = geoopt.PoincareBall()
 
-dataset = SyntheticDataset(ball, 50, 2, numberOfsiblings=100, sigma_sibling=0.05, dist_children=.7)
+dataset = SyntheticDataset(ball, 50, 2, numberOfsiblings=100,
+                           sigma_sibling=0.05, dist_children=.7)
 HXs = dataset.data.double()
-HXs =  HXs - HXs.mean(axis=0)
+HXs = HXs - HXs.mean(axis=0)
 
 ys = dataset.labels
-colors=dataset.labels.max(-1).values 
+colors = dataset.labels.max(-1).values
 
 # %%
 # Visualization of the original similarities
 # -----------------------
 #
-# We can observe the hierarchical nature of the input data by examining the 
+# We can observe the hierarchical nature of the input data by examining the
 # pairwaise distance matrix in the input space
 
-from torchdr import pairwise_distances
-D = pairwise_distances(HXs,HXs,metric='sqeuclidean')
+D = pairwise_distances(HXs, HXs, metric='sqeuclidean')
 
 plt.figure()
 plt.imshow(D)
@@ -169,12 +183,11 @@ plt.show()
 # Computing TSNE and COSNE
 # -----------------------
 #
-# We can now proceed to computing the two DR methods and visualizing 
+# We can now proceed to computing the two DR methods and visualizing
 # the results
 
-from torchdr import TSNE, COSNE
 
-myTSNE = TSNE(lr=1e-1,coeff_attraction=1,verbose=True)
+myTSNE = TSNE(lr=1e-1, coeff_attraction=1, verbose=True)
 out_tsne = myTSNE.fit_transform(HXs)
 
 myCOSNE = COSNE(lr=1e-1, verbose=True, optimizer='RAdam', gamma=.5, lambda1=0.1,
@@ -182,15 +195,13 @@ myCOSNE = COSNE(lr=1e-1, verbose=True, optimizer='RAdam', gamma=.5, lambda1=0.1,
 out_cosne = myCOSNE.fit_transform(HXs)
 
 
-from torchdr.utils.visu import plotGrid
-
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-axes[0].scatter(*out_tsne.T,c=colors, cmap= plt.get_cmap("rainbow"))
+axes[0].scatter(*out_tsne.T, c=colors, cmap=plt.get_cmap("rainbow"))
 axes[0].set_xticks([])
 axes[0].set_yticks([])
-axes[0].set_title('T-SNE',fontsize=24)
+axes[0].set_title('T-SNE', fontsize=24)
 plotGrid(axes[1])
-axes[1].scatter(*out_cosne.T,c=colors, cmap= plt.get_cmap("rainbow"))
+axes[1].scatter(*out_cosne.T, c=colors, cmap=plt.get_cmap("rainbow"))
 axes[1].axis('off')
-axes[1].set_title('CO-SNE',fontsize=24)
+axes[1].set_title('CO-SNE', fontsize=24)
 plt.show()
