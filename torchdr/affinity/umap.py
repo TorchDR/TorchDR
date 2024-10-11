fcdc19e73025f3734b10870d4150d8934c5c39ea
huguesva <vanasselhugues@gmail.com>
2024-10-11 14:39:20 +0200

    name fix umap aff message

--- torchdr/affinity/umap.py
+++ torchdr/affinity/umap.py
@@ -140,7 +140,7 @@ class UMAPAffinityIn(SparseLogAffinity):
                 warnings.warn(
                     "[TorchDR] WARNING Affinity: n_neighbors is large "
                     f"({self.n_neighbors}) thus we turn off sparsity for "
-                    "the EntropicAffinity. "
+                    "the UMAPAffinityIn. "
                 )
             return False
 
