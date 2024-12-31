c62865f1aa56fb03e92b4dd448b6de55c51689fa
huguesva <43318922+huguesva@users.noreply.github.com>
2024-12-31 13:27:19 +0100

    [Doc] new theme ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    
    ## Motivation and context / Related issue
    
    <!--- Why is this change required? What problem does it solve? -->
    
    <!--- Please link to an existing issue here if one exists. -->
    
    <!--- (we recommend to have an existing issue for each pull request) -->
    
    ## How has this been tested (if it applies)
    
    <!--- Please describe here how your modifications have been tested. -->
    
    ## PR checklist
    
    <!-- - Go over all the following points, and put an `x` in all the boxes that apply. -->
    
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
    
    - [ ] I have read the [**How to Contribute**](https://torchdr.github.io/torchdr.contributing.html) document.
    - [ ] The documentation is up-to-date with the changes I made (check build artifacts).
    - [ ] All tests passed, and additional code has been **covered with new tests**.
    - [ ] I have added the PR and Issue fix to the [**RELEASES.rst**](RELEASES.rst) file.
    
    <!--- In any case, don't hesitate to join and ask questions if you need on slack (https://pot-toolbox.slack.com/), gitter (https://gitter.im/PythonOT/community), or the mailing list (https://mail.python.org/mm3/mailman3/lists/pot.python.org/). -->

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ bibtex_default_style = "alpha"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 html_static_path = []
 html_favicon = "figures/logo.ico"
 html_logo = "figures/torchdr_logo.png"
