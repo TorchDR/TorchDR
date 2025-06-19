cd47ce47ccdeafebc9b2eb9f3589051af2caa12c
huguesva <vanasselhugues@gmail.com>
2025-06-19 16:41:44 +0000

    bugfix ruff format

--- torchdr/utils/visu.py
+++ torchdr/utils/visu.py
@@ -34,7 +34,7 @@ def plot_poincare_disk(ax, alpha=0.1):
     interior = distance <= radius
     shading = np.zeros_like(distance)
     shading[interior] = hypDistance[interior]  # Shading based on distance
-    ax.imshow(shading, extent=[-1, 1, -1, 1], cmap='Greys', alpha=alpha)
+    ax.imshow(shading, extent=[-1, 1, -1, 1], cmap="Greys", alpha=alpha)
 
 
 def plot_disk(ax, alpha=0.5):
@@ -47,10 +47,10 @@ def plot_disk(ax, alpha=0.5):
     alpha : float, optional
         The transparency level of the shaded distance background, by default 0.5.
     """
-    ax.set_aspect('equal')
+    ax.set_aspect("equal")
     ax.set_xlim([-1.05, 1.05])
     ax.set_ylim([-1.05, 1.05])
-    ax.axis('off')
+    ax.axis("off")
 
     plot_poincare_disk(ax, alpha=alpha)
     circle = plt.Circle((0, 0), 1, color="k", linewidth=1, fill=False)
