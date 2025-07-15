503ae0fd994255cd3c1aea61e1f3ad7dd9d282d4
huguesva <vanasselhugues@gmail.com>
2025-07-15 11:34:56 +0000

    faiss

--- torchdr/tests/test_utils.py
+++ torchdr/tests/test_utils.py
@@ -107,16 +107,16 @@ def test_pairwise_distances_keops(dtype, metric):
     y = torch.randn(m, p, dtype=dtype)
 
     # --- check consistency between torch and keops ---
-    C, _ = pairwise_distances(x, y, metric=metric, backend=None)
-    C_keops, _ = pairwise_distances(x, y, metric=metric, backend="keops")
+    C = pairwise_distances(x, y, metric=metric, backend=None)
+    C_keops = pairwise_distances(x, y, metric=metric, backend="keops")
     check_shape(C_keops, (n, m))
 
     check_similarity_torch_keops(C, C_keops, K=10)
 
     # --- check consistency between torch and keops with kNN search ---
     k = 10
-    C, _ = pairwise_distances(x, y, k=k, metric=metric, backend=None)
-    C_keops, _ = pairwise_distances(x, y, k=k, metric=metric, backend="keops")
+    C = pairwise_distances(x, y, k=k, metric=metric, backend=None)
+    C_keops = pairwise_distances(x, y, k=k, metric=metric, backend="keops")
     check_shape(C_keops, (n, k))
 
     torch.testing.assert_close(C, C_keops, rtol=1e-5, atol=1e-5)
@@ -133,10 +133,10 @@ def test_pairwise_distances_faiss(dtype, metric, exclude_diag):
 
     # --- check consistency between torch and faiss ---
     k = 10
-    C, _ = pairwise_distances(
+    C = pairwise_distances(
         x, y, k=k, metric=metric, backend=None, exclude_diag=exclude_diag
     )
-    C_faiss, _ = pairwise_distances(
+    C_faiss = pairwise_distances(
         x, y, k=k, metric=metric, backend="faiss", exclude_diag=exclude_diag
     )
     check_shape(C_faiss, (n, k))
