bb34a476282e5d1cd665a5971ed5aee652d5344c
huguesva <vanasselhugues@gmail.com>
2025-08-31 03:56:44 +0000

    Consolidate ExactIncrementalPCA and add proper citation
    - Move ExactIncrementalPCA into incremental_pca.py
    - Add Ross et al. (2008) citation to IncrementalPCA docstring
    - Remove redundant logger initialization
    - Both PCA variants now in same file with clear comparison

--- torchdr/spectral_embedding/__init__.py
+++ torchdr/spectral_embedding/__init__.py
@@ -1,6 +1,5 @@
 from .kernel_pca import KernelPCA
-from .incremental_pca import IncrementalPCA
-from .exact_incremental_pca import ExactIncrementalPCA
+from .incremental_pca import IncrementalPCA, ExactIncrementalPCA
 from .pca import PCA
 from .phate import PHATE
 
