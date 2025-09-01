0dce60a560f9e30845c0b070c746528c47e303d8
huguesva <vanasselhugues@gmail.com>
2025-09-01 18:07:29 +0000

    fix

--- torchdr/neighbor_embedding/pacmap.py
+++ torchdr/neighbor_embedding/pacmap.py
@@ -160,7 +160,7 @@ class PACMAP(SampledNeighborEmbedding):
         self.register_buffer("self_idxs", self_idxs, persistent=False)
 
         mid_near_indices = torch.empty(
-            self.X_.shape[0], self.n_mid_near, device=target_device
+            self.X_.shape[0], self.n_mid_near, device=target_device, dtype=torch.long
         )
         self.register_buffer("mid_near_indices", mid_near_indices, persistent=False)
         return super()._fit_transform(X, y)
@@ -226,9 +226,7 @@ class PACMAP(SampledNeighborEmbedding):
                     device=self.device,
                 )
                 _, idxs = kmin(D_mid_near_candidates, k=2, dim=1)
-                self.mid_near_indices[:, i] = idxs[
-                    :, 1
-                ]  # Retrieve the second closest point
+                self.mid_near_indices[:, i] = idxs[:, 1]
 
             Q_mid_near = 1 + pairwise_distances(
                 self.embedding_,
