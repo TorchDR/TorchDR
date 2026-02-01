259454027c81fd013170cb0f9c2582fafe1b2cfa
Hugues Van Assel <43318922+huguesva@users.noreply.github.com>
2026-01-31 18:57:05 -0800

    [Feature] Add DistributedPCA for multi-GPU PCA computation (#248)

--- torchdr/spectral_embedding/__init__.py
+++ torchdr/spectral_embedding/__init__.py
@@ -1,6 +1,14 @@
 from .kernel_pca import KernelPCA
 from .incremental_pca import IncrementalPCA, ExactIncrementalPCA
 from .pca import PCA
+from .distributed_pca import DistributedPCA
 from .phate import PHATE
 
-__all__ = ["KernelPCA", "IncrementalPCA", "ExactIncrementalPCA", "PCA", "PHATE"]
+__all__ = [
+    "KernelPCA",
+    "IncrementalPCA",
+    "ExactIncrementalPCA",
+    "PCA",
+    "DistributedPCA",
+    "PHATE",
+]
